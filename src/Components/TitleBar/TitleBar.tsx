5dd7b55b9c76dbb90c635d115c5e55cddf313196
pinu <pinuwork@gmail.com>
2023-09-01 14:56:41 +0530

    created a back button on taskbar

--- src/Components/TitleBar/TitleBar.tsx
+++ src/Components/TitleBar/TitleBar.tsx
@@ -1,18 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./TitleBar.css";
-import { useEffect } from "react";
+import { BsFillCaretLeftFill } from "react-icons/bs";
 
-interface props {
-  canvasTitle: string;
-}
+const TitleBar = () => {
+  const navigate = useNavigate();
 
-const TitleBar = ({ canvasTitle }: props) => {
   let { name } = useParams();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
   return (
     <div className="title-bar-container">
       <div className="title-bar">
+        <div onClick={handleGoHome} className="logo">
+          <BsFillCaretLeftFill />
+        </div>
         <p className="title">{name}</p>
+        <div></div>
       </div>
     </div>
   );
