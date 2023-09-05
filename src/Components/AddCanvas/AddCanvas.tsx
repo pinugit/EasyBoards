e5ad66e00b03da8cca95259361d2f8462b78358f
pinu <pinuwork@gmail.com>
2023-09-05 12:24:30 +0530

    changed the div to button in addCanvas component

--- src/Components/AddCanvas/AddCanvas.tsx
+++ src/Components/AddCanvas/AddCanvas.tsx
@@ -2,13 +2,13 @@ import "./AddCanvas.css";
 import { motion } from "framer-motion";
 const AddCanvas = () => {
   return (
-    <motion.div
+    <motion.button
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.9 }}
       className="add-canvas"
     >
       <h1>add canvas</h1>
-    </motion.div>
+    </motion.button>
   );
 };
 
